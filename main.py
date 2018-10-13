b45ecbfca8400d90d31fca2a4b3c8637e6ccf161
Nathan <nsunbury@hmc.edu>
2018-10-13 02:13:06 -0700

    2:38 AM update

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# C:\ProgramData\Anaconda3\Scripts\ipython3.exe
+# C:\Users\Nathan\hackathon2018
+
+
 import time, random
 
 RACK_SIZE = 5
 
 lockIDs = {} #lockIDs keeps a record of which position in the rack a given id has reserved
-rackSlots = [] #rackSlots keeps a record of which slots in the rack are empty and full
+racklots = [] #racklots keeps a record of which slots in the rack are empty and full
 
 class Slot:
 
@@ -15,7 +19,7 @@ class Slot:
 		#self.hasBoard = False
 		#self.isHingeOpen = False
 
-	def claim(self, id):
+	def claim(self, studentID):
 		#checks whether or not the slot physically has a board in it
 		#if self.hasBoard == False:
 
@@ -27,11 +31,10 @@ class Slot:
 			time.sleep(0.1)
 
 		#once the rack is re-closed, the states will update
-		if isHingeOpen(self.position) == True: 
-			self.isClaimed = True
-			self.isLocked = True
-			self.studentIDNum = id
-			lock_solenoid(self.position)
+		self.isClaimed = True
+		self.isLocked = True
+		self.studentIDNum = studentID
+		lock_solenoid(self.position)
 
 	def unclaim(self):
 		#Checks whether or not the slot physically has a board in it
@@ -45,11 +48,10 @@ class Slot:
 			time.sleep(0.1)
 
 		#once the rack is re-closed, the states will update
-		if isHingeOpen(self.position) == False: 
-			self.isClaimed = False
-			self.isLocked = False
-			self.studentIDNum = None
-			lock_solenoid(self.position)
+		self.isClaimed = False
+		self.isLocked = False
+		self.studentIDNum = None
+		lock_solenoid(self.position)
 
 	def __repr__(self):
 		printstr = ""
@@ -63,30 +65,43 @@ class Slot:
 		return printstr
 
 
-racks = [Slot(i) for i in range(RACK_SIZE)]
+rack = [Slot(i) for i in range(RACK_SIZE)]
 
-def find_open_rack_pos():
-	for slot in racks:
+def find_open_slot_pos():
+	for slot in rack:
 		if slot.isClaimed == False:
 			return slot.position
 
-def add_board(id):
-	slot = racks[find_open_rack_pos()]
-	slot.claim(id)
-	print("rack", slot, "claimed")
+def add_board(studentID):
+	slot = rack[find_open_slot_pos()]
+	slot.claim(studentID)
+	print(slot)
 
-def find_rack(id):
-	for slot in racks:
-		if slot.studentIDNum == id:
+def find_slot(studentID):
+	for slot in rack:
+		if slot.studentIDNum == studentID:
 			return slot
 
-def remove_board(id):
-	slot = find_rack(id)
+def remove_board(studentID):
+	slot = find_slot(studentID)
 	slot.unclaim()
-	print("rack", slot, "unclaimed")
+	print(slot)
+
+def add_remove_board(studentID):
+	# checks that the input is a number
+	try:
+		studentID = int(studentID)
+		studentID = str(studentID)
+		# loops through the rack and checks whether the input student ID is registered to any of the slots
+		for slot in rack:
+			if slot.studentIDNum == studentID:
+				remove_board(studentID)
+				add_board(studentID)
+	except:
+		return
 
 def print_rack():
-	for slot in racks:
+	for slot in rack:
 		print(slot)
 
 def isHingeOpen(position):
@@ -102,17 +117,3 @@ def unlock_solenoid(num):
 	pass
 def hasBoard(ultrasonicSensorInput):
 	pass
-
-add_board("test")
-print(find_open_rack_pos())
-add_board("test2")
-print(find_open_rack_pos())
-add_board("test3")
-print(find_open_rack_pos())
-print_rack()
-remove_board("test")
-print_rack()
-
-
-
-
